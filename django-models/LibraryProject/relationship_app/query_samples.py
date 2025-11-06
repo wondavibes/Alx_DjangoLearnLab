b48be9f2e4725cc46e6fedb45783495b087f6c9b
wondavibes <amosayemoro2001@gmail.com>
2025-11-06 13:44:03 +0100

    edited query 2

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -29,7 +29,7 @@ def get_books_in_library(library_name):
 def get_librarian_for_library(library_name):
     try:
         library = Library.objects.get(name=library_name)
-        librarian = library.librarian  # type: ignore
+        librarian = Librarian.objects.get(library=library)  # type: ignore
         print(f"Librarian for {library.name} Library: {librarian.name}")
     except Library.DoesNotExist:
         print(f"No library found with name '{library_name}'")
