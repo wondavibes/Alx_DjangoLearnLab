37dc580cc6a0c770a1c907894e00f00b6d9e4ddc
wondavibes <amosayemoro2001@gmail.com>
2025-11-27 11:06:44 +0100

    new permissions

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,10 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics, permissions, status
-from rest_framework.permissions import (
-    IsAuthenticated,
-    IsAuthenticatedOrReadOnly,
-    AllowAny,
-)
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+
 from rest_framework.response import Response
 from rest_framework.exceptions import ValidationError
 from rest_framework.request import Request as DRFRequest
