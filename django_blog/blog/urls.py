09d6bff2f7ba4726279d1d0b07d972e0c72d1d21
wondavibes <amosayemoro2001@gmail.com>
2025-12-06 11:36:49 +0100

    edited update url path

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path("", views.PostListView.as_view(), name="post_list"),
     path("post/<int:pk>/", views.PostDetailView.as_view(), name="post_detail"),
     path("post/new/", views.PostCreateView.as_view(), name="post_create"),
-    path("post/<int:pk>/edit/", views.PostUpdateView.as_view(), name="post_update"),
+    path("post/<int:pk>/update/", views.PostUpdateView.as_view(), name="post_update"),
     path("post/<int:pk>/delete/", views.PostDeleteView.as_view(), name="post_delete"),
 ]
