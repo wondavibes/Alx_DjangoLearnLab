b78258895317414c263edd1bc1917512103c8ebe
wondavibes <amosayemoro2001@gmail.com>
2025-12-12 20:14:20 +0100

    added posts to installed apps

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "accounts",
-    "rest_framework.authtoken",
+    "posts" "rest_framework.authtoken",
 ]
 
 MIDDLEWARE = [
