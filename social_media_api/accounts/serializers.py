8b545e0a1adcdfa6989d1c646c85b5a85f9a6aba
wondavibes <amosayemoro2001@gmail.com>
2025-12-12 09:39:38 +0100

    user creation logic

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -15,7 +15,7 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         # Create user with validated data
-        user = User.objects.create_user(
+        user = get_user_model().objects.create_user(
             username=validated_data["username"],
             email=validated_data["email"],
             password=validated_data["password"],
