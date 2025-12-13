0c2435cd10db9fc2b507e4335d62c3e4559b2807
wondavibes <amosayemoro2001@gmail.com>
2025-12-13 11:29:15 +0100

    feed url path

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,9 +1,13 @@
 # posts/urls.py
 from rest_framework.routers import DefaultRouter
 from .views import PostViewSet, CommentViewSet
+from django.urls import path
 
 router = DefaultRouter()
 router.register("posts", PostViewSet, basename="posts")
 router.register("comments", CommentViewSet, basename="comments")
 
+urlpatterns = [
+    path("feed/", PostViewSet.as_view({"get": "feed"}), name="post-feed"),
+]
 urlpatterns = router.urls
